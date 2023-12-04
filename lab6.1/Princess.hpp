2cef55c7e3fb4d8ef8b1cd579740992b969d1e54
Aleksandr Kovalenko <74919179+Serohon@users.noreply.github.com>
2023-12-04 10:56:41 +0300

    Update Princess.hpp

--- lab6.1/Princess.hpp
+++ lab6.1/Princess.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "NPC.hpp"
 
-struct Princess : public NPC
+struct Princess final: public NPC
 {
     Princess(int x, int y);
     Princess(std::istream &is);
@@ -9,8 +9,5 @@ struct Princess : public NPC
     void print() override;
     void save(std::ostream &os) override;
     bool accept(std::shared_ptr<NPC> attacker) override;
-    bool fight(std::shared_ptr<Dragon> other) override;
-    bool fight(std::shared_ptr<WanderingKnight> other) override;
-    bool fight(std::shared_ptr<Princess> other) override;
     friend std::ostream &operator<<(std::ostream &os, Princess &knight);
 };
