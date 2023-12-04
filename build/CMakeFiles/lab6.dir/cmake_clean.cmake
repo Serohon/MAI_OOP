9c1a2c2a4485916b16568a78ffac1aef3e2e3a8c
Aleksandr <sasha20.08.2004@yandex.ru>
2023-12-04 10:50:03 +0300

    Update Laboratory work VI

---
+++ build/CMakeFiles/lab6.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab6.dir/Dragon.cpp.o"
+  "CMakeFiles/lab6.dir/Dragon.cpp.o.d"
+  "CMakeFiles/lab6.dir/NPC.cpp.o"
+  "CMakeFiles/lab6.dir/NPC.cpp.o.d"
+  "CMakeFiles/lab6.dir/Princess.cpp.o"
+  "CMakeFiles/lab6.dir/Princess.cpp.o.d"
+  "CMakeFiles/lab6.dir/WanderingKnight.cpp.o"
+  "CMakeFiles/lab6.dir/WanderingKnight.cpp.o.d"
+  "CMakeFiles/lab6.dir/main.cpp.o"
+  "CMakeFiles/lab6.dir/main.cpp.o.d"
+  "lab6"
+  "lab6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sanya/OPP/lab6
+# Build directory: /home/sanya/OPP/lab6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BASIC "/home/sanya/OPP/lab6/build/tests")
+set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/home/sanya/OPP/lab6/CMakeLists.txt;10;add_test;/home/sanya/OPP/lab6/CMakeLists.txt;0;")
