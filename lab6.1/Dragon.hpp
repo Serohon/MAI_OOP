9aaed8c76d39459ac2a81b45366bce35050e1ed3
Aleksandr Kovalenko <74919179+Serohon@users.noreply.github.com>
2023-12-04 10:55:05 +0300

    Update Dragon.hpp

--- lab6.1/Dragon.hpp
+++ lab6.1/Dragon.hpp
@@ -1,16 +1,14 @@
 #pragma once
 #include "NPC.hpp"
 
-struct Dragon : public NPC
+struct Dragon final: public NPC
 {
     Dragon(int x, int y);
     Dragon(std::istream &is);
 
     void print() override;
-     bool accept(std::shared_ptr<NPC> attacker) override;
-    bool fight(std::shared_ptr<Dragon> other) override;
-    bool fight(std::shared_ptr<WanderingKnight> other) override;
-    bool fight(std::shared_ptr<Princess> other) override;
+    bool accept(std::shared_ptr<NPC> attacker) override;
+    bool visitPrincess(std::shared_ptr<Princess> other) override;
 
     void save(std::ostream &os) override;
 
